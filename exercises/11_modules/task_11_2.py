fc18292f4f90b3ad669b3d808024c815b6db9df5
Maksim Ivanov <megajump2009@gmail.com>
2018-06-08 17:28:50 +0300

    11_2

--- exercises/11_modules/task_11_2.py
+++ exercises/11_modules/task_11_2.py
@@ -26,3 +26,6 @@
 > pip install graphviz
 
 '''
+from task_11_1 import parse_cdp_neighbors
+from draw_network_graph import draw_topology
+draw_topology(parse_cdp_neighbors('sw1_sh_cdp_neighbors.txt'))
