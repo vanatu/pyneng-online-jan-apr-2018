b37b6a6568222dc54888cadf372f72dc9c301fd0
vanatu <megajump2009@gmail.com>
2018-06-11 13:54:13 -0700

    task 12_1 done

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -14,8 +14,21 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+import subprocess
+from pprint import pprint
+
 def check_ip_addresses(ip_list):
-	ip_ok = ip_bad = []
+	ip_ok, ip_bad = [], []
+	for ip in ip_list:
+		r = subprocess.run('ping -c 3 {}'.format(ip),
+		 					shell = True,
+							stdout=subprocess.DEVNULL)
+		if r.returncode == 0:
+			ip_ok.append(ip)
+		else:
+			ip_bad.append(ip)
+
 	return ip_ok, ip_bad
-	
-check_ip_addresses(['8.8.8.8'])
+
+if __name__ == '__main__':
+	pprint(check_ip_addresses(['8.8.8.8', '8.8.4.4', '127.0.0.1', '40.20.10.1']))
