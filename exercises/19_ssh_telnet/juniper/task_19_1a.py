30ae1557339c52f46f095204f40cb4c934ba5906
vanatu <megajump2009@gmail.com>
2018-07-25 11:01:48 +0300

    19_1a

--- exercises/19_ssh_telnet/juniper/task_19_1a.py
+++ exercises/19_ssh_telnet/juniper/task_19_1a.py
@@ -10,3 +10,24 @@
 
 Для проверки измените пароль на устройстве или в файле devices.yaml.
 '''
+import netmiko, yaml
+from pprint import pprint
+
+command = 'show version | except \['
+
+def send_show_command(device, command):
+    result = {}
+    with open(device) as f:
+        devices = yaml.load(f)
+        for d in devices['routers']:
+            try:
+                conn = netmiko.ConnectHandler(**d)
+                output = conn.send_command(command)
+                result[d['ip']] = output
+            except netmiko.ssh_exception.NetMikoAuthenticationException:
+                print('Not connect to {}. Authentication failed'.format(d['ip']))
+    return result
+
+
+if __name__ == '__main__':
+    pprint(send_show_command('devices.yaml', command))
