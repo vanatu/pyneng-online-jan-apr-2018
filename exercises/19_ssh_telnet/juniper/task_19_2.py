e641ddfbe6680c5be92899614baa8fcad8a6a57f
vanatu <megajump2009@gmail.com>
2018-07-25 13:09:23 +0300

    19_2

--- exercises/19_ssh_telnet/juniper/task_19_2.py
+++ exercises/19_ssh_telnet/juniper/task_19_2.py
@@ -4,20 +4,45 @@
 
 Создать функцию send_config_commands
 
-Функция подключается по SSH (с помощью netmiko) к устройству и выполняет перечень команд в конфигурационном режиме на основании переданных аргументов.
+Функция подключается по SSH (с помощью netmiko) к устройству и выполняет перечень команд
+в конфигурационном режиме
+на основании переданных аргументов.
 
 Параметры функции:
 * device - словарь с параметрами подключения к устройству
 * config_commands - список команд, которые надо выполнить
 
 Функция возвращает словарь с результатами выполнения команды:
-* ключ - IP устройства
-* значение - вывод с выполнением команд
-
-Отправить список команд commands на все устройства из файла devices.yaml (для этого надо считать информацию из файла) с помощью функции send_config_commands.
+* ключ - команда
+* значение - вывод с выполнением команды
 
 '''
 
 commands = [
-    'logging 10.255.255.1', 'logging buffered 20010', 'no logging console'
+    'delete routing-instances v1998-TEST',
 ]
+
+import netmiko
+
+DEVICE = {
+    'device_type': 'juniper',
+    'ip': '10.251.15.251',
+    'username': 'lab',
+    'password': 'lab123',
+    'verbose': False,
+}
+
+def send_config_command(device, config_commands):
+    net_connect = netmiko.ConnectHandler(**device)
+    print(net_connect.find_prompt())
+    print(net_connect.config_mode(config_command='configure private'))
+    if net_connect.check_config_mode():
+        net_connect.send_config_set(config_commands, exit_config_mode=False)
+        output = net_connect.send_config_set("show | compare", exit_config_mode=False)
+        output += net_connect.commit(and_quit=True)
+        print(output)
+    
+    net_connect.disconnect()
+
+if __name__ == '__main__':
+    send_config_command(DEVICE, commands)
