aa30fee27f00dade2501a457a2cddc8e26379725
vanatu <megajump2009@gmail.com>
2018-07-30 13:21:31 +0300

    task_21_1a

--- exercises/21_jinja2/task_21_1a.py
+++ exercises/21_jinja2/task_21_1a.py
@@ -15,3 +15,20 @@
 * lstrip_blocks
 
 '''
+from jinja2 import Environment, FileSystemLoader
+import yaml, sys, os
+
+
+def generate_cfg_from_template(path_template, path_vars_yaml, trim=True, lstrip=True):
+    TEMPLATE_DIR, template_file = os.path.split(path_template)
+    vars_dict = yaml.load(open(path_vars_yaml))
+
+    env = Environment(
+        loader=FileSystemLoader(TEMPLATE_DIR),
+        trim_blocks=trim,
+        lstrip_blocks=lstrip)
+    template = env.get_template(template_file)
+    
+    return template.render(vars_dict)
+
+print(generate_cfg_from_template('templates/for.txt', 'data_files/for.yml', lstrip=False))
