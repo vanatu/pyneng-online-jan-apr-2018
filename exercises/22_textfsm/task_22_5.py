67d0fd22191cc77713dae4694bfb29c70f3d7dc1
vanatu <megajump2009@gmail.com>
2018-08-02 16:47:30 +0300

    task_22_5

--- exercises/22_textfsm/task_22_5.py
+++ exercises/22_textfsm/task_22_5.py
@@ -31,3 +31,20 @@
 
 Проверить работу функции send_and_parse_command на команде sh ip int br.
 '''
+import sys
+sys.path.append('/home/vanatu/pyneng-online-jan-apr-2018/exercises/19_ssh_telnet/juniper')
+from task_19_1 import send_show_command
+from task_22_4 import parse_command_dynamic
+
+def send_and_parse_command(devices, command):
+    result = {}
+    attr = {'Command': command}
+    output = send_show_command(devices, command)
+    for ip, out in output.items():
+        result[ip] = parse_command_dynamic(attr, out, tmpl_dir='templates_jun')
+    return result
+
+
+if __name__ == '__main__':
+    command = 'show version'
+    print(send_and_parse_command('devices_jun.yaml', command))
